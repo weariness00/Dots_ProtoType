cb496005515cb526d1f966649f2f4c4f11157433
Go In Ho <58318769+k0301h@users.noreply.github.com>
2023-10-13 12:52:09 +0900

    인호꺼 처럼 벤치마크 수정

--- Assets/Kimhoyoung/Script/makecube.cs
+++ Assets/Kimhoyoung/Script/makecube.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 
 public class makecube : MonoBehaviour
 {
     public GameObject prefab;
     public int count;
 
+    private float deltaTime;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +19,18 @@ public class makecube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < count; ++i)
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+
+        if (fps > 60)
         {
-            Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.Euler(0f, 0f, 0f));
+            for (int i = 0; i < count; ++i)
+            {
+                var random = UnityEngine.Random.Range(-360, 360);
+
+                Instantiate(prefab, new Vector3(math.cos(random), 0, math.sin(random)), Quaternion.Euler(0f, 0f, 0f));
+            }
         }
-        
+
     }
 }
--- Assets/Kimhoyoung/Script/movecube.cs
+++ Assets/Kimhoyoung/Script/movecube.cs
@@ -8,18 +8,22 @@ public class movecube : MonoBehaviour
     private float angle = 0f;
 
     public float Speed;
-    public float Radian;
+    
+    private float x;
+    private float y;
+    private float z;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        x = transform.position.x;
+        z = transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        angle += Time.deltaTime * Speed;
-        gameObject.transform.position = new Vector3(math.cos(angle) * Radian, 0, math.sin(angle) * Radian);
+        // gameObject.transform.position.x = new Vector3(math.cos(angle) * Radian, 0, math.sin(angle) * Radian);
+        transform.position += new Vector3(x * Time.deltaTime * Speed, 0, z * Time.deltaTime * Speed);
     }
 }
